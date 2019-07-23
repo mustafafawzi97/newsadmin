c1020bb7d620276b94955edd0532dabbd69d7a16
Rami Saad <ra997mi@gmail.com>
2019-07-23 04:03:44 +0300

    http requests

--- src/app/services/web.service.ts
+++ src/app/services/web.service.ts
@@ -1,10 +1,48 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';;
+import { Login } from '../services/models/login.model';
+import { Observable } from 'rxjs';
+import { Articles } from './models/articles.model';
+import { Notify } from './models/notify.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebService {
 
-  constructor() { }
+  SERVER_URL = 'http://localhost:8080';
 
+  constructor(private http: HttpClient) { }
+
+  checkLogin(login: Login): Observable<any> {
+    return this.http.post(`${this.SERVER_URL}/news/api/login.php`, { data: login });
+  }
+
+  getArticles(): Observable<Articles[]> {
+    return this.http.get<Articles[]>(`${this.SERVER_URL}/news/api/articles/get.php`);
+  }
+
+  getSingleArticle(id: Number): Observable<Articles> {
+    return this.http.get<Articles>(`${this.SERVER_URL}/news/api/articles/get_single.php?id=${id}`);
+  }
+
+  addArticle(article: Articles): Observable<any> {
+    return this.http.post(`${this.SERVER_URL}/news/api/articles/add.php`, { data: article });
+  }
+  UpdateArticle(article: Articles): Observable<any> {
+    return this.http.post(`${this.SERVER_URL}/news/api/articles/update.php`, { data: article });
+  }
+  deleteArticle(id: Number): Observable<any> {
+    return this.http.post(`${this.SERVER_URL}/news/api/articles/delete.php`, { data: { 'id': id } });
+  }
+
+  getNotify(): Observable<Notify[]> {
+    return this.http.get<Notify[]>(`${this.SERVER_URL}/news/api/notify/get.php`);
+  }
+  addNotify(notify: Notify): Observable<any> {
+    return this.http.post(`${this.SERVER_URL}/news/api/notify/add.php`, { data: notify });
+  }
+  deleteNotify(id: Number): Observable<any> {
+    return this.http.post(`${this.SERVER_URL}/news/api/notify/delete.php`, { data: { 'id': id } });
+  }
 }
