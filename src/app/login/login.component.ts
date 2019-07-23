b51a0b6cad4c2492363209d16d6d4dd0d15416c9
Rami Saad <ra997mi@gmail.com>
2019-07-23 04:04:45 +0300

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { SESSION_STORAGE, StorageService } from 'angular-webstorage-service';
 import { ToastrService } from 'ngx-toastr';
+import { Login } from '../services/models/login.model';
 const STORAGE_KEY = 'local_user';
 
 @Component({
@@ -15,14 +16,15 @@ export class LoginComponent implements OnInit {
 
   show: boolean = false;
 
-  password() {
+  getPassword() {
     this.show = !this.show;
   }
 
   constructor(private webService: WebService, private router: Router,
     @Inject(SESSION_STORAGE) private storage: StorageService,
     private toastr: ToastrService,
-    private spinnerService: NgxSpinnerService) { }
+    private spinnerService: NgxSpinnerService,
+    private login: Login) { }
 
   ngOnInit() {
     this.spinnerService.show();
@@ -32,12 +34,17 @@ export class LoginComponent implements OnInit {
     }, 2000);
   }
 
-
-  signIn(f) {
-    // LOGIN AUTHONICATION
-    // SAVE SESSION OR LOCAL KEY? MAYBE TOKEN
-    // BELLOW IS TEMP ONLY!
-    this.storage.set(STORAGE_KEY, 'userin');
-    this.router.navigate(['articles']);
+  signIn() {
+    this.webService.checkLogin(this.login).subscribe(data => {
+      console.log(data['exist'])
+      if (data['exist'] != undefined && data['exist'] == true) {
+        this.storage.set(STORAGE_KEY, data['exist']);
+        this.router.navigate(['articles']);
+      }
+    }, error => {
+      if (error.status == 422) {
+        this.toastr.show("البريد الالكتروني او كلمة المرور غير صحيحة", "خطأ");
+      }
+    });
   }
 }
